287967b216ad3ffb5de6e22fe39b719c83460c98
Thilo Utke <thilo@upstre.am>
2012-10-05 13:51:48 +0200

    facial is working

--- stalk.js
+++ stalk.js
@@ -1,19 +1,70 @@
-var cv = require('../lib/opencv');
-var http    = require('http');
-var fs      = require('fs');
 
-var camera = new cv.VideoCapture(0);
+var http = require('http');
+var cv = require('opencv');
+var fs      = require('fs');
+var arDrone = require('ar-drone');
+var log = console.log;
+
+
+log('Connecting png stream ...');
+
+
 var COLOR = [0, 255, 0]; //default red
 var thickness = 2; // default 1
-var lastPng;
+var lastPngBuffer;
 var mimeTypes = {
   'html':    'text/html',
   'js':      'text/javascript'
 };
 
+var pngStream = arDrone.createPngStream();
+
+var lastPng;
+pngStream
+  .on('error', log)
+  .on('data', function(pngBuffer) {
+    lastPngBuffer = pngBuffer;
+});
+
+
+var detectFace = function(){
+  cv.readImage(lastPngBuffer, function(err, im){
+     log("Size: " + im.width() + "x" + im.height());
+     im.resize(im.width() / 2, im.height() / 2);
+     log("Resize: " + im.width() + "x" + im.height());
+
+     im.detectObject('./data/haarcascade_frontalface_alt2.xml', {}, function(err, faces) {
+
+       for(var k = 0; k < faces.length; k++) {
+
+         face = faces[k];
+         im.rectangle([face.x, face.y], [face.x + face.width, face.y + face.height], COLOR, 2);
+       }
+
+            /*var methods = []
+            for(var j in im) {
+                if(typeof im[j] === "function") methods.push(j);
+            }
+            log(methods.sort().join("\n"));*/
+       lastPng = im.toBuffer();
+     });
+   });
+};
+
+var faceDetectLoop = function(){
+  
+  if(lastPngBuffer){
+    log('Detecting Face.');
+    detectFace();
+  };
+  setTimeout(faceDetectLoop, 500);
+};
+faceDetectLoop();
+
 var server = http.createServer(function(req, res) {
   
   if (req.url === '/cam.png') {
+  
     if (!lastPng) {
       res.writeHead(503);
       res.end('Did not receive any png data yet.');
@@ -29,51 +80,16 @@ var server = http.createServer(function(req, res) {
     var fileType = file.replace(/\.\w+$/) || "html";
     
     fs.readFile(file, function (err, content) {
-      if (err) { throw err; }
+      if (err) { log(err); }
       res.writeHead(200, {'Content-Type': mimeTypes[fileType]});
       res.end(content);
     });
-    /*fs.exists(file, function (exists) {
-      if (!exists) {
-        res.writeHead(404);
-        res.end('File not found');
-        return;
-      }
-    });*/
     
   }
   
 });
 
 server.listen(8080, function() {
-  console.log('Serving latest png on port 8080 ...');
+  log('Serving latest png on port 8080 ...');
 });
 
-setInterval(function() {
-  var start = new Date().getTime();
-	camera.read(function(im) {
-	  console.log("Size: " + im.width() + "x" + im.height());
-	  im.resize(im.width() / 2, im.height() / 2);
-	  console.log("Resize: " + im.width() + "x" + im.height());
-    
-		im.detectObject('../data/haarcascade_frontalface_alt2.xml', {}, function(err, faces) {
-
-    		for(var k = 0; k < faces.length; k++) {
-
-    			face = faces[k];
-    			im.rectangle([face.x, face.y], [face.x + face.width, face.y + face.height], COLOR, 2);
-    		}
-
-            /*var methods = []
-            for(var j in im) {
-                if(typeof im[j] === "function") methods.push(j);
-            }
-            console.log(methods.sort().join("\n"));*/
-    		lastPng = im.toBuffer();
-    		var end = new Date().getTime();
-    		console.log("Time: " + (end - start));
-
-    	});
-	});
-
-}, 250);
